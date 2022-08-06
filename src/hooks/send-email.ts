8e4a6b9884980ea5cd2d6591874b8df322014ac5
Marek Smetanka <smetmarek@gmail.com>
2022-08-06 21:09:30 +0200

    Emails hotfix

--- src/hooks/send-email.ts
+++ src/hooks/send-email.ts
@@ -5,13 +5,21 @@ import { createTransport } from "nodemailer";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (options = {}): Hook => {
   return async (context: HookContext): Promise<HookContext> => {
+    const users = context.app.services.users.Model;
+
     const createTran = createTransport({
       host: "smtp.example.net",
       port: 587,
       secure: false, // upgrade later with STARTTLS
       auth: {
         user: "example@test.sk",
-        pass: "password",
+        pass: "D(R1u600=kR6qP-;s",
+      },
+    });
+
+    const userEmail = await users.findAll({
+      where: {
+        id: context.data.userId,
       },
     });
 
@@ -20,18 +28,17 @@ export default (options = {}): Hook => {
         console.log(error);
       } else {
         console.log("Server is ready to take our messages");
+        const message = {
+          from: "example@test.sk",
+          to: userEmail[0].toJSON().email,
+          subject: "You have been added to a todo list",
+          text: `You have been added to a todo list check url: https://example.com/todo/${context.data.todoId}`,
+          html: `<p>You have been added to a todo list list check url: https://example.com/todo/${context.data.todoId}</p>`,
+        };
+
+        createTran.sendMail(message);
       }
     });
-
-    const message = {
-      from: "example@test.sk",
-      to: "reciver@test.sk",
-      subject: "You have been added to a todo list",
-      text: `You have been added to a todo list check url: https://example.com/todo/${context.data.todoId}`,
-      html: `<p>You have been added to a todo list list check url: https://example.com/todo/${context.data.todoId}</p>`,
-    };
-
-    createTran.sendMail(message);
 
     return context;
   };
